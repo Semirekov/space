085b596be52d6cebc785a9b59b6d221dc95e43ad
Semirekov Evgenii <ekar@yandex.ru>
2022-08-20 06:35:06 +0300

    Вынес chat_id в настройки окружения.

--- bot.py
+++ bot.py
@@ -16,17 +16,23 @@ def create_parser():
     return parser
 
 
-async def upload_file(token, filename):
-    
+async def upload_file(token, chat_id, filename):    
     bot = telegram.Bot(token)
     async with bot:        
-        await bot.send_document(chat_id='@spacefanat', document=open(filename, 'rb'))        
+        await bot.send_document(
+            chat_id=chat_id,
+            document=open(filename, 'rb')
+        )        
 
 
 def upload_image(filename):    
     load_dotenv()    
     asyncio.run(
-        upload_file(os.environ['TELEGRAM_TOKEN'], filename)
+        upload_file(
+            os.environ['TELEGRAM_TOKEN'],
+            os.environ['CHAT_ID'],
+            filename
+        )
     )
 
 
@@ -35,7 +41,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     load_dotenv()    
     asyncio.run(
-        upload_file(os.environ['TELEGRAM_TOKEN'], args.filename)
+        upload_file(
+            os.environ['TELEGRAM_TOKEN'],
+            os.environ['CHAT_ID'],
+            args.filename
+        )
     )
 
 
